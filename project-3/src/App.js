d921355babc03912d3588650664eff2f21237095
emanuele <emanueca@stud.ntnu.no>
2019-10-10 16:48:05 +0200

    #8 Styling changes in ListRow component

--- project-3/src/App.js
+++ project-3/src/App.js
@@ -4,12 +4,12 @@ import List from '././components/List/List.js'
 
 const data = [
   {name:"McDonald",address:"Torgetvei 3",city:"Trondheim",smiley:2,rating:1},
-  {name:1,address:2,city:323,smiley:2, rating:3},
-  {name:2,address:3,city:2423,smiley:1, rating:2},
-  {name:3,address:4,city:234,smiley:0, rating:5},
-  {name:4,address:5,city:233,smiley:0, rating:5},
-  {name:5,address:6,city:23,smiley:2, rating:3},
-  {name:6,address:7,city:233,smiley:1, rating:4}
+  {name:"Burger King",address:2,city:"Bergen",smiley:2, rating:3},
+  {name:"Villa paradiso",address:3,city:"Oslo",smiley:0, rating:5},
+  {name:"Narvesen",address:"Stavangervei",city:"Stavanger",smiley:0, rating:5},
+  {name:"Una Pizzeria",address:"something",city:"Trondheim",smiley:0, rating:5},
+  {name:"Egon",address:6,city:"Oslo",smiley:2, rating:3},
+  {name:"Seven Eleven",address:7,city:"Alta",smiley:1, rating:4}
 ];
 
 function App() {
