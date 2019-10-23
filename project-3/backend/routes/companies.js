8bd0c5651bd6e0d5f62621d75352997b9a37d61e
harkamalsi <harkamal_guraya@hotmail.com>
2019-10-23 14:40:02 +0200

    #22 #23 Made a new api endpoint and implemented pagination in backend with skip-limit.

--- project-3/backend/routes/companies.js
+++ project-3/backend/routes/companies.js
@@ -1,88 +1,122 @@
 const express = require('express');
 const router = express.Router();
-
+var ObjectId = require('mongodb').ObjectId;
 const Company = require('../models/company.model');
 
-// @route     GET companies
-// @desc      Get all companies with possible sorts and filters on zipcode and city
-// @access    Public
-router.route('/').get((req, res) => {
+const getQuery = (apiquery, mapDataRoute) => {
+  // apiquery is an request object
+  // mapDataRoute is a boolean
+
   let orderNameInt;
   let orderSmileyInt;
-  let apiquery = req.query;
 
   let name = apiquery.name;
   let orderby = apiquery.orderby;
   let cities = apiquery.cities;
   let smileys = apiquery.smileys;
 
-  // 1 = ASC, -1 = DESC
-  switch (orderby) {
-    case 'NAME_AZ':
-      orderNameInt = 1;
-      break;
-    case 'NAME_ZA':
-      orderNameInt = -1;
-      break;
-    case 'SMILEY_ASC':
-      // [3, 2, 1, 0] = "Sur, nøytral, smil, smil"
-      // Because of how mongoDb works, we must provide with -1
-      orderSmileyInt = -1;
-      break;
-    case 'SMILEY_DESC':
-      // [0, 1, 2, 3] = "Smil, smil, nøytral, Sur"
-      // Because of how mongoDb works, we must provide with 1
-      orderSmileyInt = 1;
-      break;
-    default:
-      // Default case is NAME_AZ
-      orderNameInt = 1;
-  }
+  let page = apiquery.page;
 
-  let mongoQuery;
-
-  if (name && !cities && !smileys) {
-    mongoQuery = {
-      name: new RegExp(name, 'i')
-    };
-  } else if (!cities && !smileys) {
-    mongoQuery = {
-      name: new RegExp(name, 'i')
-    };
-  } else if (!cities) {
-    mongoQuery = {
-      name: new RegExp(name, 'i'),
-      'smileys.0.grade': { $in: smileys.split('-').map(Number) }
-    };
-  } else if (!smileys) {
-    mongoQuery = {
-      name: new RegExp(name, 'i'),
-      city: { $in: cities.split('-') }
-    };
-  } else {
-    //(name || cities || smileys)
-    mongoQuery = {
-      name: new RegExp(name, 'i'),
-      city: { $in: cities.split('-') },
-      'smileys.0.grade': { $in: smileys.split('-').map(Number) }
-    };
-  }
-
-  let mongoSortQuery;
-
-  if (orderSmileyInt) {
+  if (!mapDataRoute) {
     // 1 = ASC, -1 = DESC
-    mongoSortQuery = { 'smileys.0.grade': orderSmileyInt };
+    switch (orderby) {
+      case 'NAME_AZ':
+        orderNameInt = 1;
+        break;
+      case 'NAME_ZA':
+        orderNameInt = -1;
+        break;
+      case 'SMILEY_ASC':
+        // [3, 2, 1, 0] = "Sur, nøytral, smil, smil"
+        // Because of how mongoDb works, we must provide with -1
+        orderSmileyInt = -1;
+        break;
+      case 'SMILEY_DESC':
+        // [0, 1, 2, 3] = "Smil, smil, nøytral, Sur"
+        // Because of how mongoDb works, we must provide with 1
+        orderSmileyInt = 1;
+        break;
+      default:
+        // Default case is NAME_AZ
+        orderNameInt = 1;
+    }
+
+    let mongoQuery;
+    if (!cities && !smileys) {
+      mongoQuery = {
+        // Finds even if name is blank
+        name: new RegExp(name, 'i')
+      };
+    } else if (!cities) {
+      mongoQuery = {
+        name: new RegExp(name, 'i'),
+        'smileys.0.grade': { $in: smileys.split('-').map(Number) }
+      };
+    } else if (!smileys) {
+      mongoQuery = {
+        name: new RegExp(name, 'i'),
+        city: { $in: cities.split('-') }
+      };
+    } else {
+      //(name || (cities && smileys))
+      mongoQuery = {
+        name: new RegExp(name, 'i'),
+        city: { $in: cities.split('-') },
+        'smileys.0.grade': { $in: smileys.split('-').map(Number) }
+      };
+    }
+
+    let mongoSearchSortQuery = {};
+    let mongoPaginationQuery = {};
+    let mongoSkipInt = 0; // Default, skips nothing
+
+    if (orderSmileyInt) {
+      // 1 = ASC, -1 = DESC
+      mongoSearchSortQuery = { 'smileys.0.grade': orderSmileyInt };
+    } else {
+      // 1 = ASC, -1 = DESC
+      mongoSearchSortQuery = { name: orderNameInt };
+    }
+
+    if (page) {
+      mongoSkipInt = parseInt(page * 10);
+    }
+
+    return [mongoQuery, mongoSearchSortQuery, mongoSkipInt];
   } else {
-    // 1 = ASC, -1 = DESC
-    mongoSortQuery = { name: orderNameInt };
+    return {
+      $project: {
+        _id: 0,
+        name: 1,
+        coordinates: 1
+      }
+    };
   }
+};
 
-  console.log(mongoSortQuery);
+// @route     GET companies
+// @desc      Get all companies with possible sorts and filters on zipcode and city
+// @access    Public
+router.route('/').get((req, res) => {
+  // queries = [mongoQuery, mongoSearchSortQuery, mongoSkipInt]
+  let queries = getQuery(req.query, false);
 
-  Company.find(mongoQuery)
-    .sort(mongoSortQuery)
+  Company.find(queries[0])
+    .sort(queries[1])
+    .skip(queries[2])
     .limit(10)
+    .then(companies => res.json(companies))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+// @route     GET mapData
+// @desc      Get all mapData: names and coordinates
+// @access    Public
+router.route('/locations').get((req, res) => {
+  // query = {mongoQuery}
+  let query = getQuery(req.query, true);
+
+  Company.aggregate([query])
     .then(companies => res.json(companies))
     .catch(err => res.status(400).json('Error: ' + err));
 });
@@ -109,30 +143,6 @@ router.route('/giverating').put((req, res) => {
 // @access    Public
 router.route('/cities').get((req, res) => {
   Company.aggregate([{ $group: { _id: null, cities: { $addToSet: '$city' } } }])
-
-    .then(company => res.json(company))
-    .catch(err => res.status(400).json('Error: ' + err));
-});
-
-// @route     GET companies/name
-// @desc      Get all companies with same name or name that includes a substring
-// @access    Public
-router.route('/name').get((req, res) => {
-  let name = req.body.name;
-
-  Company.find({ name: new RegExp(name, 'i') })
-    .limit(5)
-    .then(company => res.json(company))
-    .catch(err => res.status(400).json('Error: ' + err));
-});
-
-// @route     GET companies/smilies
-// @desc      Get companies with given smiley grade(s) given at last review
-// @access    Public
-router.route('/smileys').get((req, res) => {
-  let smileysArr = req.body.smileys;
-
-  Company.find({ 'smileys.0.grade': { $in: smileysArr } })
     .then(company => res.json(company))
     .catch(err => res.status(400).json('Error: ' + err));
 });
