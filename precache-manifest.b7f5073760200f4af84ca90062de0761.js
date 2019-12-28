42e788bea3bcd5c089231b49294d558e13fc0bfd
harkamalsi <harkamal_guraya@hotmail.com>
2019-12-28 16:01:41 +0100

    Updates

---
+++ precache-manifest.b7f5073760200f4af84ca90062de0761.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59d2dfa1aa6b0ddb04d061ae11efaec0",
+    "url": "/spisested-advisor/index.html"
+  },
+  {
+    "revision": "76ba96c2ac046eb2ceb0",
+    "url": "/spisested-advisor/static/css/2.5ffb5b65.chunk.css"
+  },
+  {
+    "revision": "e2461f6ecdad03c74c9b",
+    "url": "/spisested-advisor/static/css/main.52e5934f.chunk.css"
+  },
+  {
+    "revision": "76ba96c2ac046eb2ceb0",
+    "url": "/spisested-advisor/static/js/2.14b8f1a9.chunk.js"
+  },
+  {
+    "revision": "e2461f6ecdad03c74c9b",
+    "url": "/spisested-advisor/static/js/main.232ee2e0.chunk.js"
+  },
+  {
+    "revision": "7789e0459349baed8f7e",
+    "url": "/spisested-advisor/static/js/runtime-main.bb3c4024.js"
+  },
+  {
+    "revision": "2379110d8bdfe1d26cc2de835a701133",
+    "url": "/spisested-advisor/static/media/mg.2379110d.svg"
+  },
+  {
+    "revision": "5c3526963644d2e9f2472a2b8d120557",
+    "url": "/spisested-advisor/static/media/smiley-happy.5c352696.svg"
+  },
+  {
+    "revision": "8821be19ab246cf8f5f568ba8398efb9",
+    "url": "/spisested-advisor/static/media/smiley-neutral.8821be19.svg"
+  },
+  {
+    "revision": "d6a4195b1cbfcab43d566864e8a85340",
+    "url": "/spisested-advisor/static/media/smiley-sad.d6a4195b.svg"
+  },
+  {
+    "revision": "e0b7262fb55a58f187610e1cda89137b",
+    "url": "/spisested-advisor/static/media/star.e0b7262f.svg"
+  }
+]);
